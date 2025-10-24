b0973af6dc8a5140fd546858fd404b011214b161
Omkar Belekar <obelekar1000-tech@users.noreply.github.com>
2025-10-24 21:11:32 +0530

    cnc

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // static export
-  basePath: '/Audumbar_Hostel_P1',
-  assetPrefix: '/Audumbar_Hostel_P1/',
+  // basePath: '/Audumbar_Hostel_P1',
+  // assetPrefix: '/Audumbar_Hostel_P1/',
   trailingSlash: true,   // ✅ important for GitHub Pages
   images: { unoptimized: true },
   eslint: { ignoreDuringBuilds: true },
