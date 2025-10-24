cd527c37c5826e18b3a636e1c0215393f26921b5
Omkar Belekar <obelekar1000-tech@users.noreply.github.com>
2025-10-24 18:31:00 +0530

    sads

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,16 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   eslint: {
-//     ignoreDuringBuilds: true,
-//   },
-//   typescript: {
-//     ignoreBuildErrors: true,
-//   },
-//   images: {
-//     unoptimized: true,
-//   },
-// }
 const nextConfig = {
-  output: 'export',                    // static export
-  basePath: '/Audumbar_Hostel_P1',     // repo name — EXACT match
-  assetPrefix: '/Audumbar_Hostel_P1/', // ensures _next static and assets are requested under repo path
-  trailingSlash: true,                 // helpful for GitHub Pages
-  images: { unoptimized: true },
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
-}
+  output: 'export',
+  basePath: '/Audumbar_Hostel_P1',
+  images: {
+    unoptimized: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  assetPrefix: '/Audumbar_Hostel_P1/',
+};
 export default nextConfig
